9ff59293669d4d2139baf01907f199b1197a4fd8
wei <wei02919@gmail.com>
2023-11-17 20:25:46 +0800

    add button for randomly generate color

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ let color1 = document.querySelector(".color1")
 let color2 = document.querySelector(".color2")
 let body = document.getElementsByTagName("BODY")[0]
 let h3 = document.getElementsByTagName("H3")[0]
+let button = document.getElementsByTagName("button")[0]
 
-function setBackgroundColor(){
-  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`
+function setBackgroundColor(color1, color2){
+  body.style.background = `linear-gradient(to right, ${color1}, ${color2})`
   h3.textContent = body.style.background 
 }
 
-color1.addEventListener("input",setBackgroundColor)
+color1.addEventListener("input",()=>setBackgroundColor(color1.value,color2.value))
 
-color2.addEventListener("input", setBackgroundColor)
+color2.addEventListener("input", ()=>setBackgroundColor(color1.value,color2.value))
+
+button.addEventListener("click", ()=>{
+  let color1 = '#' + Math.floor(Math.random()*16777215).toString(16)
+  let color2 = '#' + Math.floor(Math.random()*16777215).toString(16)
+  setBackgroundColor(color1, color2)
+})
+
